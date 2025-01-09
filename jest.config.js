42a29881b2720e2336a79245892c569eb31efd63
Matthias-Zax <matthiaszax@gmail.com>
2025-01-09 12:44:37 +0100

    fix: resolve Jest configuration conflicts

--- jest.config.js
+++ jest.config.js
@@ -3,4 +3,15 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   testMatch: ['**/tests/**/*.supertest.ts'],
+  reporters: [
+    'default',
+    [
+      'jest-html-reporter',
+      {
+        pageTitle: 'SuperTest API Test Report',
+        outputPath: 'test-report.html',
+        includeFailureMsg: true
+      }
+    ]
+  ]
 };
